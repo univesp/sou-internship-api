3795b15d67aea181b81a63595478dbab8c0c83dc
Robson Arruda <robson.arruda@connectcom.univesp.br>
2018-12-18 11:46:03 -0200

    Adds process controller and changes route

---
+++ app/controllers/api/v1/process_controller.rb
@@ -0,0 +1,57 @@
+class Api::V1::ProcessController < ApplicationController
+	def index
+		@process = {
+			"process": [
+				{
+					"id": 1,
+					"type": 0,
+					"status": 0,
+					"created_at": "2018-12-17",
+					"student_id": 1,
+					"responsible_id": 1,
+					"grantor_id": 1,
+					"advisor_id": 1,
+					"professor_id": 1,
+					"moderator_name": "Tânia Soares de Souza"
+				},
+				{
+					"id": 2,
+					"type": 0,
+					"status": 0,
+					"created_at": "2018-12-17",
+					"student_id": 1,
+					"responsible_id": 1,
+					"grantor_id": 1,
+					"advisor_id": 1,
+					"professor_id": 1,
+					"moderator_name": "Tânia Soares de Souza"
+				},
+				{
+					"id": 3,
+					"type": 0,
+					"status": 0,
+					"created_at": "2018-12-17",
+					"student_id": 1,
+					"responsible_id": 1,
+					"grantor_id": 1,
+					"advisor_id": 1,
+					"professor_id": 1,
+					"moderator_name": "Rafael da Silva" 
+				},
+				{
+					"id": 4,
+					"type": 0,
+					"status": 0,
+					"created_at": "2018-12-17",
+					"student_id": 1,
+					"responsible_id": 1,
+					"grantor_id": 1,
+					"advisor_id": 1,
+					"professor_id": 1,
+					"moderator_name": "Rafael da Silva" 
+				}
+			]
+		}
+		render json: @process
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       # get 'grantor/:id',  to: 'grantor#reset',
-      resources :grantor, :professor, :responsible, :advisor
+      resources :grantor, :professor, :responsible, :advisor, :process
     end
   end
 end
