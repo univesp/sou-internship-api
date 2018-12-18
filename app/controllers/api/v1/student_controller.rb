b9e89ee1c83c099937106ea34c4f16323e110e0d
Thiago Calura <thiagocalura@outlook.com>
2018-12-18 14:39:29 -0200

    changes student mock for processes

--- app/controllers/api/v1/student_controller.rb
+++ app/controllers/api/v1/student_controller.rb
@@ -1,33 +1,33 @@
 class Api::V1::StudentController < ApplicationController
 
-  # "aluno/:id/processos", to "student#processes"
+  # "student/:id/processes", to "student#processes"
   # 0 = 'Em análise', 1 = 'Deferido',  2 = 'Indeferido' 3 = 'Pendente' (references for status)
   def processes
 		@processes = {
 			[
-			  {
+        {
           "id": 1,
           "type": 0,
           "status": 0,
           "created_at": "2018-12-17",
-          "responsible": {
-            "name": "Tânia Cunha Castro"
-          },
-          "grantor": {
-            "name": "Universidade de São Paulo"
-          }
-        }, 
+          "student_id": 1,
+          "responsible_id": 1,
+          "grantor_id": 1,
+          "advisor_id": 1,
+          "professor_id": 1,
+          "moderator_name": "Marcos Ferreira Almeida"
+        },
         {
           "id": 2,
           "type": 0,
           "status": 1,
-          "created_at": "2018-12-17",
-          "responsible": {
-            "name": "Marcos Ferreira Almeida"
-          },
-          "grantor": {
-            "name": "Universidade de São Paulo"
-          }
+          "created_at": "2018-12-20",
+          "student_id": 1,
+          "responsible_id": 1,
+          "grantor_id": 1,
+          "advisor_id": 1,
+          "professor_id": 1,
+          "moderator_name": "Tânia Cunha Castro"
         }
 			]
     }
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
 
       get "student/:id", to: "student#student_data"
+      get "student/:id/processes", to: "student#processes"
+
       # get 'grantor/:id',  to: 'grantor#reset',
       resources :grantor, :professor, :responsible, :advisor, :process
     end
