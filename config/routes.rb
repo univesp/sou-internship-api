ed19a409d7a88821866aff0f2f76ac6a7153de83
Robson Arruda <robson.arruda@connectcom.univesp.br>
2018-12-24 12:03:14 -0200

    Changes route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "base#index"
   namespace :api do
     get "/", to: "base#index"
-    namespace :v1 do
+    namespace :v1, defaults: { format: :json } do
 
       get "student/:id", to: "student#student_data"
       get "student/:id/processes", to: "student#processes"
